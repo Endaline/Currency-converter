6ea98362695882c71d3af81705863da3751223db
Endaline <endalineihuoma7@gmail.com>
2022-09-05 15:14:54 +0100

    currency converter

--- currency/src/App.js
+++ currency/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import React from 'react';
 import Convert from './Components/Convert';
-import Convert2 from './Components/Convert2';
 
 function App() {
   return (
     <div className="App">
       
       <Convert />
-      <Convert2/>
      
 
     </div>
